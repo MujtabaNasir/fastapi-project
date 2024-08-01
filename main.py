a442a899057485cee5ec05efced9312fdb7c75fc
MujtabaNasir <mujtaba.nasir1312@gmail.com>
2024-08-01 18:42:00 +0500

    Code duplication removed. Job done with just one endpoint now.

--- main.py
+++ main.py
@@ -4,21 +4,19 @@ import uvicorn
 from fastapi import FastAPI
 from mujtaba_charm.utils.sample import hello
 
-app = FastAPI()
-
 logging.basicConfig(
     level=logging.INFO, format="[%(asctime)s] %(levelname)s in %(module)s: %(message)s"
 )
 logger = logging.getLogger(__name__)
 
+app = FastAPI()
+
 
 @app.get("/hello")
-@app.get("/hello/{name}")
 async def get_hello(name: str = None):
     """
-    Prints a greeting message.
-
-    If a name parameter is provided, it greets that name; otherwise, it greets the world.
+    Prints a greeting message. If a name is provided, it greets the name;
+    otherwise, it greets the world.
 
     Args:
         name (str, optional): The name to greet. Defaults to None.
@@ -26,13 +24,8 @@ async def get_hello(name: str = None):
     Returns:
         dict: A dictionary containing a greeting message.
     """
-    if name:
-        logger.info(f"Handling /hello/{name} request")
-        message = hello(name)
-    else:
-        logger.info("Handling /hello request")
-        message = hello()
-
+    logger.info(f"Handling /hello request")
+    message = hello(name or "World")
     logger.info(f"Returning message: {message}")
     return {"message": message}
 
